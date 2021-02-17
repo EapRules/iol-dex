9eefedebdab2f280e876b761d13dc781ae888b8f
EapRules <enzoapoliti@gmail.com>
2021-02-17 02:13:24 -0300

    lang fixed

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,16 +1,19 @@
-import React,{useContext}from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { ThemeContext } from "../../Context/ThemeContext";
+import { useTranslation } from "react-i18next";
 
 function Pagination({ prevPage, nextPage, pageNum }) {
-const {theme} = useContext(ThemeContext)
+
+    const { t } = useTranslation();
+    const { theme } = useContext(ThemeContext)
 
     return (
         <ButtonGroup>
-            <Button variant={theme.backgroundColor === "black" ? "dark" : "primary"} onClick={prevPage} disabled={!prevPage}>Previous</Button>
+            <Button variant={theme.backgroundColor === "black" ? "dark" : "primary"} onClick={prevPage} disabled={!prevPage}>{t("PrevPage")}</Button>
             <Button variant={theme.backgroundColor === "black" ? "dark" : "primary"} disabled>{pageNum}</Button>
-            <Button variant={theme.backgroundColor === "black" ? "dark" : "primary"} onClick={nextPage} disabled={!nextPage}>Next</Button>
+            <Button variant={theme.backgroundColor === "black" ? "dark" : "primary"} onClick={nextPage} disabled={!nextPage}>{t("NextPage")}</Button>
         </ButtonGroup>
     );
 }
